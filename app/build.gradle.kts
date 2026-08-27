2e0c1f38ba6530c4bb59dc3c3cb22cc4dd3c6441
mdmac2683 <mdmac1983@proton.me>
2026-08-26 22:33:21 -0400

    Add ExifInterface dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,5 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.exifinterface:exifinterface:1.3.7")
 }
